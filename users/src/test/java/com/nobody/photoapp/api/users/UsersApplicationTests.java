fd137e7b0702401642b9ab13979d5895885e8836
EL HASSANE OUMINA <e.oumina@gmail.com>
2024-07-13 11:59:21 +0200

    Spring Boot MicroServices with Spring Cloud API Gateway

---
+++ users/src/test/java/com/nobody/photoapp/api/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobody.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
